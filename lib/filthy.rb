0d14ca9785c6389aed91ca151a18f0362248f4c9
Mike 'sealabcore' Taylor <miketaylor@me.com>
2012-01-19 23:31:19 -0800

    use class_variable_set to set class variables in a module

--- lib/filthy.rb
+++ lib/filthy.rb
@@ -1,39 +1,48 @@
+require 'active_record'
+require 'active_record/version'
+require 'active_support'
+
 module Filthy
 
   module ClassMethods
 
     def filthy_attributes(*args)
-      @@filthy_attributes = args
-
-      @@filthy_attribute_symbol_array = @@filty_attributes.collect { |attribute| :"#{attribute}_filthy" }
-
-      attr_accessor @@filthy_attribute_symbol_array
+      class_variable_set(:@@filthy_attributes, args)
+            
+      class_variable_set(:@@filthy_attribute_methods, class_variable_get(:@@filthy_attributes).collect { |attribute| :"#{attribute}_filthy" })
+      
+      class_variable_get(:@@filthy_attribute_methods).each do |method|
+        attr_accessor method
+        
+        send(:define_method, :"#{method}?") do
+          !!send(method)
+        end
+      end
     end
-
   end
 
   module InstanceMethods
-
-    def set_filty_before_save
-      showerand_and_scrub_filthy_attributes
-      filthy_attributes = changes.keys.select { |attribute| @@filthy_attributes.include? attribute.to_s }
-      filthy_attributes.each do |fa|
-        send("#{fa.to_s}_filthy=", true)
+    
+    def set_filthy_before_save
+      clean_filthy_attributes
+      filthy_attributes = changes.keys.select { |attribute| self.class.send(:class_variable_get, :@@filthy_attributes).include? attribute.to_sym }
+      filthy_attributes.each do |filthy_attribute|
+        send("#{filthy_attribute}_filthy=", true)
       end
     end
 
-    def showerand_and_scrub_filthy_attributes
-      @@filthy_attribute_symbol_array.each { |fa| send("#{fa}_filthy=", nil) }
+    def clean_filthy_attributes
+      self.class.send(:class_variable_get, :@@filthy_attribute_methods).each { |fa| send("#{fa}=", nil) }
     end
 
   end
 
   def self.included(base)
     base.extend ClassMethods
-    base.include InstanceMethods
-    base.before_save :set_filty_before_save
+    base.send(:include, InstanceMethods)
+    base.before_save :set_filthy_before_save    
   end
-
+  
 end
 
 class ActiveRecord::Base
